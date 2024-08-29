a55287b691a8747f1ff0603e74a98a171fd8c4c8
RamRam-V <cajhyung@gmail.com>
2024-08-29 23:22:25 +0900

    feat: refresh token 기능 추가

--- biz/lib/authOptions.ts
+++ biz/lib/authOptions.ts
@@ -11,10 +11,52 @@ import { signIn } from "@/biz/api/users-api";
 
 declare module "next-auth/jwt" {
     interface JWT {
-        id_token?: string;
-        provider?: string;
+        id_token: string;
+        provider: string;
+        refresh_token: string;
+        accessTokenExpires: number;
+        error: string;
     }
 }
+
+async function refreshAccessToken(token: JWT) {
+    try {
+        const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            body: new URLSearchParams({
+                client_id: process.env.GOOGLE_CLIENT_ID!,
+                client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+                grant_type: "refresh_token",
+                refresh_token: token.refresh_token!,
+            }),
+        });
+
+        const tokensOrError = await response.json();
+
+        if (!response.ok) throw tokensOrError;
+
+        const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+        };
+
+        token.access_token = newTokens.access_token;
+        token.expires_at = Math.floor(Date.now() / 1000 + newTokens.expires_in);
+        // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+        if (newTokens.refresh_token)
+            token.refresh_token = newTokens.refresh_token;
+        return token;
+    } catch (error) {
+        console.log(error);
+
+        return {
+            ...token,
+            error: "RefreshAccessTokenError",
+        };
+    }
+}
+
 export const authOptions: AuthOptions = {
     providers: [
         // KeycloakProvider({
@@ -26,22 +68,35 @@ export const authOptions: AuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             checks: ["none"],
+            authorization: {
+                params: { access_type: "offline", prompt: "consent" },
+            },
         }),
     ],
     callbacks: {
         async jwt({ token, user, account, profile }) {
             if (account) {
-                token.id_token = account.id_token;
-                token.provider = account.provider;
+                token.id_token = account.id_token!;
+                token.provider = account.provider!;
+                token.refresh_token = account.refresh_token!;
+
+                token.accessTokenExpires =
+                    Date.now() + account.expires_at! * 1000;
             }
 
-            return token;
+            if (Date.now() < token.accessTokenExpires) {
+                return token;
+            }
+
+            // Access token has expired, try to update it
+            return refreshAccessToken(token);
         },
         async session({ session, token }) {
             session.user = {
                 ...session.user,
                 id_token: token.id_token,
             };
+            session.error = token.error;
 
             return session;
         },
--- biz/types/next-auth.d.ts
+++ biz/types/next-auth.d.ts
@@ -5,6 +5,8 @@ declare module "next-auth" {
      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
      */
     interface Session {
+        error: string;
+
         user: {
             /** The user's postal address. */
             // image?: string;
